b882c7a8016406696a1d239c31a11253c446a72f
Syed Hassan Abbas <hassanabbassayed@gmail.com>
2019-02-08 20:15:05 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+import {Router, Route} from "react-router";
 
 class App extends Component {
+  state = {
+     person: [
+       {name: "Max", age: 25},
+       {name: "Shah", age: 22},
+       {name: "Clara", age: 26}
+     ]
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({person: [
+        {name: "Max", age: 25},
+        {name: event.target.value, age: 22},
+        {name: "Clara", age: 26}
+      ]
+    })
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was Clicked!');
+    this.setState({person: [
+        {name: newName, age: 25},
+        {name: "Shah", age: 22},
+        {name: "Clara", age: 26}
+      ]
+    })
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi! I am a React Developer</h1>
+        <p>This is really working!</p>
+        <button onClick={() => this.switchNameHandler("Maximilian!!")}>Switch Name</button>
+        <Person
+            name={this.state.person[0].name}
+            age={this.state.person[0].age}/>
+        <Person
+            name={this.state.person[1].name}
+            age={this.state.person[1].age}
+            click={this.switchNameHandler.bind(this, "Max!")}
+            changed={this.nameChangeHandler}>My Hobbies: Racing</Person>
+        <Person
+            name={this.state.person[2].name}
+            age={this.state.person[2].age}/>
+
+
       </div>
     );
+    //return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Now I\'m a h1 Tag'));
   }
 }
 
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Home {
+    Render(){
+        return(
+          <div>
+
+          </div>
+        );
+    }
+}
